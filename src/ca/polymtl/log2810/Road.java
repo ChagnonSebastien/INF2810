3cbfb656349f68504fa974eab0ffbbcaadd0207d
Unknown <chagnon.s21@gmail.com>
2018-02-21 16:39:16 -0500

    Implemented the road class

--- src/ca/polymtl/log2810/Road.java
+++ src/ca/polymtl/log2810/Road.java
@@ -1,5 +1,28 @@
 package ca.polymtl.log2810;
 
 public class Road {
+		
+	private int length;
+	
+	private City[] endCities;
 
+	public Road(int length) {
+		this.length = length;
+	}
+	
+	public City[] addCity(City city) {
+		if (this.endCities.length >= 2)
+			throw new ArrayIndexOutOfBoundsException("This road already has two end cities.");
+		
+		this.endCities[this.endCities.length] = city;
+		return this.endCities;
+	}
+	
+	public int getLength() {
+		return this.length;
+	}
+	
+	public City[] getEndCities() {
+		return this.endCities;
+	}
 }
