2265449e1a26c1b40708dc0cf745983ee7b18277
Unknown <chagnon.s21@gmail.com>
2018-02-21 17:20:39 -0500

    Implemented the City class

--- src/ca/polymtl/log2810/City.java
+++ src/ca/polymtl/log2810/City.java
@@ -2,4 +2,29 @@ package ca.polymtl.log2810;
 
 public class City {
 
+	private int id;;
+	private String name;
+	private boolean gazStation;
+	
+	City (int id, boolean gazStation) {
+		this.id = id;
+		
+		try {
+			this.name = CityName.getCityById(id).name;
+		} catch (IndexOutOfBoundsException e) {
+			this.name = String.valueOf(id);
+		}
+	}
+	
+	public boolean hasGazStation () {
+		return this.gazStation;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
 }
