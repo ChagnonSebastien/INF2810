ac2acf557317ee2ee42e63b3d1364bd8821b7479
Unknown <chagnon.s21@gmail.com>
2018-02-21 17:40:46 -0500

    Implemented Graph class

--- src/ca/polymtl/log2810/Graph.java
+++ src/ca/polymtl/log2810/Graph.java
@@ -2,4 +2,37 @@ package ca.polymtl.log2810;
 
 public class Graph {
 
+	private City[] cities;
+	private Road[] roads;
+	
+	public Graph() {
+		
+	}
+	
+	public City[] getCities() {
+		return this.cities;
+	}
+	
+	public Road[] getRoads() {
+		return roads;
+	}
+	
+	public void createCity(int id, boolean gazStation) throws Exception {
+		if (this.getCity(id) != null)
+			throw new Exception("A city with this id already exists.");
+		
+		this.cities[this.roads.length] = new City(id, gazStation);
+	}
+	
+	public void createRoad(int length, int[] endCities) {
+		
+	}
+	
+	public City getCity(int id) {
+		for (City city : this.cities)
+			if (city.getID() == id)
+				return city;
+		
+		return null;
+	}
 }
