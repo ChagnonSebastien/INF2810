163d25ec7bee8c00c3f0444ed7949292317b25fe
Unknown <chagnon.s21@gmail.com>
2018-02-21 16:51:44 -0500

    Added a City name enum with all the cities

---
+++ src/ca/polymtl/log2810/CityName.java
@@ -0,0 +1,32 @@
+package ca.polymtl.log2810;
+
+public enum CityName {
+	MONTREAL(1, "Montréal"),
+	QUEBEC(2, "Québec"),
+	OTTAWA(3, "Ottawa"),
+	TORONTO(4, "Totonto"),
+	HALIFAX(5, "Halifax"),
+	SEPT_ILE(6, "Sept-Ile"),
+	THUNDER_BAY(7, "Thunder Bay"),
+	SANDY_LAKE(8, "Sandy Lake"),
+	WINNIPEG(9, "Winnipeg"),
+	REGINA(10, "Regina"),
+	SASKATOON(11, "Saskatoon"),
+	CALGARY(12, "Calgary"),
+	VANCOUVER(13, "Vancouver"),
+	EDMONTON(14, "Edmonton"),
+	FORT_MCMURRAY(15, "Fort McMurray"),
+	CHURCHILl(16, "Churchill"),
+	PRINCE_GEORGE(17, "Prince George"),
+	FORT_NELSON(18, "Fort Nelson"),
+	WHITEHORSE(19, "Whitehorse"),
+	YELLOWKNIFE(20, "Yellowknife");
+	
+	private int id;
+	private String name;
+	
+	CityName(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+}
