7ee75b1184ebfc2c832dfca6a4c675a7889387b3
Unknown <chagnon.s21@gmail.com>
2018-02-21 17:41:55 -0500

    Removed the addEndCity method from the road class as it is now in the constructor

--- src/ca/polymtl/log2810/Road.java
+++ src/ca/polymtl/log2810/Road.java
@@ -6,16 +6,9 @@ public class Road {
 	
 	private City[] endCities;
 
-	public Road(int length) {
+	public Road(int length, City[] endCities) {
 		this.length = length;
-	}
-	
-	public City[] addCity(City city) throws IndexOutOfBoundsException {
-		if (this.endCities.length >= 2)
-			throw new IndexOutOfBoundsException("This road already has two end cities.");
-		
-		this.endCities[this.endCities.length] = city;
-		return this.endCities;
+		this.endCities = endCities;
 	}
 	
 	public int getLength() {
