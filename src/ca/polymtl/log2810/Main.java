747bd80de31b8cd9fe273f1d3669131b56f83d4a
Unknown <chagnon.s21@gmail.com>
2018-02-24 21:16:59 -0500

    Basic startup in the main method

--- src/ca/polymtl/log2810/Main.java
+++ src/ca/polymtl/log2810/Main.java
@@ -1,9 +1,29 @@
 package ca.polymtl.log2810;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		try {
+			GraphCreater.createGraph(new File("villes.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		JFrame frame = new JFrame("Escape the cops");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        frame.setSize(1000, 750);
+        frame.setVisible(true);
 	}
 
 }
