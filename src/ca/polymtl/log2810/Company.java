eb4b308a639cd89b83d12ed4fd02f182f3648267
Unknown <chagnon.s21@gmail.com>
2018-02-27 17:29:14 -0500

    Added 3 parameters representing the gaz consumption for each car in the Company enum

--- src/ca/polymtl/log2810/Company.java
+++ src/ca/polymtl/log2810/Company.java
@@ -1,11 +1,18 @@
 package ca.polymtl.log2810;
 
 public enum Company {
-	CHEAP_CAR(),
-	SUPER_CAR();
+	CHEAP_CAR(0.05, 0.07, 0.08),
+	SUPER_CAR(0.03, 0.04, 0.06);
 	
-	private char voiture; // v - voiture, p - pick-up, f - fourgon. 
+	private double carConsumation;
+	private double pickUpConsumation;
+	private double vanConsumation;
 	
+	Company(double carConsumation, double pickUpConsumation, double vanConsumation) {
+		this.carConsumation = carConsumation;
+		this.pickUpConsumation = pickUpConsumation;
+		this.vanConsumation = vanConsumation;
+	}
 }
 	 
 
